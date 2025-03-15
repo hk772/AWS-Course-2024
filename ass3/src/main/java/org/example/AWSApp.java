a7d262393d0317c8f6a997a4391447df2fe2992e
hagai <hagaik@post.bgu.ac.il>
2025-03-15 21:59:38 +0200

    zip

--- ass3/src/main/java/org/example/AWSApp.java
+++ ass3/src/main/java/org/example/AWSApp.java
@@ -20,7 +20,7 @@ public class AWSApp {
     public static int numberOfInstances = 9;
     private static String region = "us-west-2";
 //    private static String region = "us-east-1";
-    public static String bucketName = "my-bucket-mevuzarot-ass3-asd";
+    public static String bucketName = "my-bucket-mevuzarot-ass2-asd";
     private static String jarName = "ass3.jar";
 //    private static String jarName = "ass3local.jar";
     public static boolean isLocal = false;
@@ -105,8 +105,8 @@ public class AWSApp {
         //Job flow
         JobFlowInstancesConfig instances = new JobFlowInstancesConfig()
                 .withInstanceCount(numberOfInstances)
-                .withMasterInstanceType(InstanceType.M4Large.toString())
-                .withSlaveInstanceType(InstanceType.M4Large.toString())
+                .withMasterInstanceType("m7i.large")
+                .withSlaveInstanceType("m7i.large")
                 .withHadoopVersion("3.4.0")
 //                .withEc2KeyName("vockey")
                 .withKeepJobFlowAliveWhenNoSteps(false)
